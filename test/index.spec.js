820619346e28a392f0430c2e8eb23eb2f27d2501
neomaxzero <maxicespedes7@hotmail.com>
2017-02-08 20:41:54 -0300

    added sinon test and workaround for performance object

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 import sastre from '../lib';
 
-function TestComponent(props) {
-    return(
-        <div>{props.name}</div>
-    );
+class TestComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div>{this.props.name}</div>)  
+    }
 };
+
+const Wrappeado = sastre(TestComponent);
+
 
 function WrappedTest() {
     return sastre(<TestComponent name="item"/>)
 }
 
 describe('react-perf-hoc', () => {
-    it('Render', () => {     
-        // console.log(testComponent({name:'max'}));
-        const wrapper = mount( <sastre />            
-        );
-        console.log(wrapper.html());        
+    it('Render', () => { 
+        const cSpai = sinon.spy(console, 'info');
+        // console.log(sinon.stub())
+        // console.log(TestComponent({name:'max'}));
+        // const wrapper = mount(<WrappedTest />);
+        // const wrapper = mount(<TestComponent />);
+        const wrapper = mount(<Wrappeado />);
+        console.log(cSpai.args.length);
+
+        // console.log(performance.now());
+        // console.log('wrapper.props', wrapper.props);   
+        // console.log(wrapper.html());        
+        // console.log(stdout);
+        // console.log(process.hrtime());
     });
 })
